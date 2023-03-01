154e2e91c7bd91bbc2b791a8c184f6ca37009c3b
Balkar_Singh <balkar81199@gmial.com>
2023-03-01 16:27:13 +0530

    feat: add filter options

--- src/components/body/index.jsx
+++ src/components/body/index.jsx
@@ -3,26 +3,40 @@ import './body.css';
 import { makeRequest } from  '../../utils';
 import { GET_EVENTS } from '../../constants/apiEndPoints';
 import Card from '../card';
+import Filter from '../filter';
 const Body = () => {
   const [events,setEvents]=React.useState();
+  const [show, setShow] = React.useState();
   const [error, setError] = React.useState();
+  const [dropdown, setDropdown] = React.useState(false);
+  const [all, setAll] = React.useState(true); 
+  const [registered, setRegistered] = React.useState(false);
+  const [bookmarked, setBookmarked] = React.useState(false);
+  const [seats, setSeats] = React.useState(false);
+
   React.useEffect(() => {
     makeRequest(GET_EVENTS, {})
       .then(response => {
         response.sort((a, b) => a.datetime < b.datetime ? -1 : 1);
         setEvents(response);
+        setShow(response);
       })
       .catch(err => {
         setError(err);
       });
   }, []);
   if (error) { return <h1>Error</h1>; }
+
+ 
   return events
     ? (
-      <div className='card-list outer-padding' >
-        {events.map((event) => (
-          <Card key={event.id} event={event} />
-        ))}
+      <div className='body'>
+        <Filter setAll={setAll} setRegistered={setRegistered} setBookmarked={setBookmarked} setSeats={setSeats} setShow={setShow} events={events} dropdown={dropdown} setDropdown={setDropdown} all={all} registered={registered} bookmarked={bookmarked} seats={seats}/>
+        <div className='card-list outer-padding' >
+          {show.map((event) => (
+            <Card key={event.id} event={event} />
+          ))}
+        </div>
       </div>
     )
     : <h1>Loading</h1>;
---
+++ src/components/filter/index.jsx
@@ -0,0 +1,99 @@
+import * as React from 'react';
+import './filter.css';
+import PropTypes from 'prop-types';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faChevronDown, faFilter, faSearch} from '@fortawesome/free-solid-svg-icons';
+import { faCircle, faCircleDot } from '@fortawesome/free-regular-svg-icons';
+const Filter = ({ setAll, setRegistered, setBookmarked, setSeats, setShow, events , dropdown, setDropdown, all ,registered, bookmarked, seats}) => {
+  
+  const handleClick = (id) => () => {
+    if(id===1){
+      setAll(true);
+      setRegistered(false);
+      setBookmarked(false);
+      setSeats(false);
+      setShow(events);
+    }
+    else if(id===2){
+      setAll(false);
+      setRegistered(true);
+      setBookmarked(false);
+      setSeats(false);
+      setShow(events.filter(event => event.isRegistered));
+    }
+    else if(id===3){
+      setAll(false);
+      setRegistered(false);
+      setBookmarked(true);
+      setSeats(false);
+      setShow(events.filter(event => event.isBookmarked));
+    }
+    else if(id===4){
+      setAll(false);
+      setRegistered(false);
+      setBookmarked(false);
+      setSeats(true);
+      setShow(events.filter(event => event.areSeatsAvailable));
+    } 
+  };
+    
+  return (
+    <div >
+      <div className='search-filter outer-padding'>
+        <div className='filter'>
+          <FontAwesomeIcon icon={faFilter}/>
+          <p>FILTER</p>
+          <FontAwesomeIcon icon={faChevronDown} onClick={()=> setDropdown(!dropdown)} />
+        </div>
+        <div className='search'>
+          <input type="text" />
+          <div className='icon'>
+            <FontAwesomeIcon icon={faSearch} />
+          </div>
+        </div>
+      </div>
+  
+      {dropdown && <div className='dropdown-content outer-padding' >
+        <div className='line'>
+          <div className='option'>
+            <FontAwesomeIcon icon={ all ? faCircleDot : faCircle } onClick={handleClick(1)}/>
+            <p>ALL</p>
+          </div>
+          <div className='option2'>
+            <p>REGISTERED</p>
+            <FontAwesomeIcon icon={registered ? faCircleDot : faCircle} onClick={handleClick(2)}/>
+          </div>
+        </div>
+        <div className='line'>
+          <div className='option'>
+            <FontAwesomeIcon icon={bookmarked ? faCircleDot : faCircle} onClick={handleClick(3)}/>
+            <p>BOOKMARKED</p>
+          </div>
+          <div className='option2'>
+            <p>SEATS AVAILABLE</p>
+            <FontAwesomeIcon icon={seats ? faCircleDot : faCircle} onClick={handleClick(4)}/>
+          </div>
+        </div>
+      </div>}
+    </div>
+  );
+};
+
+
+Filter.propTypes = {
+  setAll: PropTypes.func.isRequired,
+  setRegistered: PropTypes.func.isRequired,
+  setBookmarked: PropTypes.func.isRequired,
+  setSeats: PropTypes.func.isRequired,
+  setShow: PropTypes.func.isRequired,
+  events: PropTypes.array.isRequired,
+  dropdown: PropTypes.bool.isRequired,
+  setDropdown: PropTypes.func.isRequired,
+  all: PropTypes.bool.isRequired,
+  registered: PropTypes.bool.isRequired,
+  bookmarked: PropTypes.bool.isRequired,
+  seats: PropTypes.bool.isRequired,
+    
+};
+
+export default Filter;
